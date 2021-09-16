b70c9e4a5d4f6926b4b0c960133df37a2eadc313
munich99 <regerstrasse@icloud.com>
2021-09-16 22:17:30 +0200

    shows menues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Daily } from './daily'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cuisine';
+
+
+  dailies : Daily[] =[{
+    menue: 'Ma-Po Tofu mit Hühnerhackfleisch',
+    preis: 8.5},
+    {
+    menue: 'Bärlauchtasche',
+    preis: 3,
+  }];
+
+  choicies : Daily[] =[];
+
+  deleteDaily(daily:Daily) : void{
+    this.dailies = this.dailies.filter( a => a != daily)
+  }
+
+  putDaily(daily:Daily) {
+    this.choicies.push(daily);
+    this.deleteDaily(daily);
+  }
 }
---
+++ src/app/daily.ts
@@ -0,0 +1,5 @@
+export interface Daily {
+    menue:string,
+    preis: number
+
+}
