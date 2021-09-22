502d5b9190bf7800dadd9e79977a37cfb7527b02
munich99 <regerstrasse@icloud.com>
2021-09-22 14:37:30 +0200

    komplett menü

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { PreviewComponent } from './preview/preview.component';
 import { DatePipeComponent } from './date-pipe/date-pipe.component';
 import { LoginComponent } from './login/login.component';
+import { MenueComponent } from './menue/menue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreviewComponent,
     DatePipeComponent,
-    LoginComponent
+    LoginComponent,
+    MenueComponent
   ],
   imports: [
     BrowserModule
