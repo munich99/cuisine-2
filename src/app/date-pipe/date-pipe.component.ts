58d6e6e09882f441ccf599d90426f7f67d592d3e
munich99 <regerstrasse@icloud.com>
2021-10-06 18:21:37 +0200

    calendar forward-backward

--- src/app/date-pipe/date-pipe.component.ts
+++ src/app/date-pipe/date-pipe.component.ts
@@ -8,14 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class DatePipeComponent implements OnInit {
 
   todayDate = new Date();
-  tomorrow =  this.todayDate.getDate()+2;
+  
+  // tomorrow =  this.todayDate.getDate()+2;
   weekday =  this.todayDate.getDay();
-
+  startDay = new Date().getDay();
+  
   wochentag = ['Sonntag','Montag','Dienstag','Mittwoch','Donnerstag','Freitag','Samstag' ];
+
+ 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // tomorrow = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+  
+// new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+
+  nextDay(){
+    
+    if(this.weekday <6) {
+      this.todayDate =  new Date(this.todayDate.setDate(this.todayDate.getDate() + 1))
+      this.weekday =  this.weekday + 1;
+      
+      
+    }
+    // this.todayDate = this.todayDate + 1;
+    
+  }
+  beforeDay(){
+
+    
+    if(this.weekday > this.startDay) {
+      this.todayDate =  new Date(this.todayDate.setDate(this.todayDate.getDate() - 1))
+      this.weekday =  this.weekday - 1;
+      console.log("tt");
+    }
   }
 
 }
